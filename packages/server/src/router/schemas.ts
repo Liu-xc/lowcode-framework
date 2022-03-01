44074fb6f3e2bb08862a3bc9aa98ea32fc1a2c0e
Liu-xc <1392166232@qq.com>
2022-03-02 00:23:40 +0800

    feat: api

--- packages/server/src/router/schemas.ts
+++ packages/server/src/router/schemas.ts
@@ -7,7 +7,7 @@ const router = new Router({
 
 router.get('/:schemaName', async (ctx: RouterContext<any, Koa.Context>, next: Koa.Next) => {
   ctx.body = 'hello';
-  console.table(ctx.params);
+  console.log(ctx.url);
   await next();
 });
 
